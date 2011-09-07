4e167278ec0dc7ae4faa73969758bcff8f140689
Konstantin Pribluda <konstantin.pribluda@gmail.com>
2011-09-07 22:17:57 +0200

    failing tests for setten inhection as well stripped down jar generation - metadata is too big for android

---
+++ src/main/java/de/pribluda/android/andject/InjectPreference.java
@@ -0,0 +1,8 @@
+package de.pribluda.android.andject;
+
+/**
+ * designates field  as target for preference value injection
+ * @author Konstantin Pribluda
+ */
+public @interface InjectPreference {
+}
--- src/test/java/de/pribluda/android/andject/ViewInjectionTest.java
+++ src/test/java/de/pribluda/android/andject/ViewInjectionTest.java
@@ -1,6 +1,7 @@
 package de.pribluda.android.andject;
 
 import android.app.Activity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import mockit.Deencapsulation;
@@ -9,6 +10,7 @@ import mockit.Mocked;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 
 /**
@@ -43,6 +45,8 @@ public class ViewInjectionTest {
 
         assertEquals(textView, Deencapsulation.getField(injectable, "asView"));
         assertEquals(button, Deencapsulation.getField(injectable, "button"));
+        assertNull(Deencapsulation.getField(injectable, "notInjected"));
+
     }
 
 
@@ -52,6 +56,8 @@ public class ViewInjectionTest {
         private android.view.View asView;
         @InjectView(id = 555)
         private Button button;
+        // shall be left alone
+        private View notInjected = null;
 
     }
 
@@ -67,7 +73,6 @@ public class ViewInjectionTest {
             {
                 injectable.findViewById(239);
                 returns(button);
-                injectable.asView = button;
 
                 injectable.findViewById(555);
                 returns(textView);
@@ -84,4 +89,82 @@ public class ViewInjectionTest {
     }
 
 
+    /**
+     * shall inject view into setter
+     */
+    @Test
+    public void testThatSetterInjectionWorksProperly(@Mocked final WithInjectableMethods injectable,
+                                                     @Mocked final TextView textView,
+                                                     @Mocked final Button button) {
+          new Expectations() {
+            {
+                injectable.findViewById(239);
+                returns(textView);
+
+
+                injectable.findViewById(555);
+                returns(button);
+
+
+            }
+        };
+
+        Injector.startActivity(injectable);
+
+        assertEquals(textView, Deencapsulation.getField(injectable, "asView"));
+        assertEquals(button, Deencapsulation.getField(injectable, "button"));
+        assertNull(Deencapsulation.getField(injectable, "notInjected"));
+    }
+
+
+    /**
+     * shall produce wiring exception
+     */
+    @Test
+    public void testThatSetterInjectionIsBombedProperlyOnNonAssignability(@Mocked final WithInjectableMethods injectable,
+                                                                          @Mocked final TextView textView,
+                                                                          @Mocked final Button button) {
+          new Expectations() {
+            {
+                injectable.findViewById(239);
+                returns(button);
+
+                injectable.findViewById(555);
+                returns(textView);
+            }
+        };
+        try {
+            Injector.startActivity(injectable);
+            fail("was expecting exception to be thrown");
+        } catch (WiringException ex) {
+            // anticipated, assure proper text
+            assertEquals("private android.widget.Button de.pribluda.android.andject.ViewInjectionTest$WithInjectableViews.button is not assignable from view id 555 (android.widget.TextView)", ex.getMessage());
+        }
+    }
+
+
+    class WithInjectableMethods extends Activity {
+
+        private android.view.View asView;
+
+        private Button button;
+
+        // shall be left alone
+        private View notInjected = null;
+        // shall be injected
+
+        @InjectView(id = 239)
+        public void setAsView(View asView) {
+            this.asView = asView;
+        }
+
+        @InjectView(id = 555)
+        public void setButton(Button button) {
+            this.button = button;
+        }
+
+        public void setNotInjected(View notInjected) {
+            this.notInjected = notInjected;
+        }
+    }
 }
